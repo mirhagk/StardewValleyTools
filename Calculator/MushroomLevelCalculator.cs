afe2ce0e1ec72c0197886e9ebe880f5681cc0bda
Nathan Jervis <mirhagk@gmail.com>
2017-02-05 10:28:42 -0500

    Added Mushroom Level Calculator
    Finds when you'll get mushroom levels in a mine

---
+++ Calculator/MushroomLevelCalculator.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class MushroomLevelCalculator
+    {
+        readonly SaveFileManager saveFileManager;
+        readonly InputHelper inputHelper = new InputHelper();
+        public MushroomLevelCalculator(SaveFileManager saveFileManager)
+        {
+            this.saveFileManager = saveFileManager;
+        }
+        public void Run(Program.Args args)
+        {
+            var save = saveFileManager.PromptToSelectSave();
+            var numDaysToCheck = inputHelper.GetInteger("Please enter how many days you want to look for (defaults to 7)", defaultNum: 7);
+            for (uint i = 0; i < numDaysToCheck; i++)
+            {
+                var day = i + save.DaysPlayed;
+                for(int level = 81; level < 120; level++)
+                {
+                    if (IsLevelAMushroomLevel(save.UniqueID, day, level))
+                    {
+                        if (i == 0)
+                            Console.WriteLine($"You will find a mushroom level at level {level} today");
+                        else
+                            Console.WriteLine($"You will find a mushroom level at level {level} in {i} days");
+                    }
+                }
+            }
+        }
+        bool IsLevelAMushroomLevel(ulong gameID, uint daysPlayed, int level)
+        {
+            var random = new Random((int)(daysPlayed + (uint)level + (uint)((int)gameID / 2)));
+            if (random.NextDouble() < 0.3)
+            {
+                random.NextDouble();
+            }
+            if (random.NextDouble() < 0.15 && level > 5 && level != 120) { }
+            if (random.NextDouble() < 0.035 && (level >= 80 && level <= 120) && level % 5 != 0)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -11,6 +11,7 @@ namespace Calculator
         public class Args
         {
             public bool Shard { get; set; }
+            public bool Mushroom { get; set; }
         }
         static void Main(string[] args)
         {
@@ -19,12 +20,16 @@ namespace Calculator
                 return;
             if (arguments.Shard)
                 RunShard(arguments);
+            else if (arguments.Mushroom)
+                RunMushroom(arguments);
             else
             {
                 OptionSelector.Create<Action<Args>>("Calculate when you'll find prismatic shards", RunShard)
+                    .AddOption("Calculate when you'll find a mushroom level in the mines", RunMushroom)
                     .Select()(arguments);
             }
         }
         static void RunShard(Args args)=> new PrismaticShardCalculator(new SaveFileManager()).Run(args);
+        static void RunMushroom(Args args) => new MushroomLevelCalculator(new SaveFileManager()).Run(args);
     }
 }
